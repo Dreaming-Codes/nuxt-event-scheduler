f46a58614b323edba2509333a588e81870a56522
DreamingCodes <dev@dreaming.codes>
2023-01-08 06:32:16 +0100

    Added email domain check and id to client JWT

--- README.md
+++ README.md
@@ -16,6 +16,7 @@ AUTH_SECRET=secret
 GOOGLE_CLIENT_ID=...
 GOOGLE_CLIENT_SECRET=...
 BASE_URL=http://localhost:3000
+EMAIL_DOMAIN=@itismeucci.com
 ```
 
 ## Development Server
--- server/api/auth/[...].ts
+++ server/api/auth/[...].ts
@@ -1,13 +1,52 @@
 import GoogleProvider from 'next-auth/providers/google'
 import {NuxtAuthHandler} from "#auth";
+import {PrismaClient} from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 export default NuxtAuthHandler({
     secret: process.env.AUTH_SECRET,
+    callbacks: {
+        async jwt({token, account, profile}) {
+            if (account && profile && profile.email) {
+                const user = await prisma.user.upsert({
+                    where: {
+                        email: profile.email
+                    },
+                    update: {},
+                    create: {
+                        name: profile.name,
+                        email: profile.email,
+                    }
+                })
+
+                token.id = user.id;
+            }
+
+            return token;
+        },
+
+        async session({session, token}){
+            // @ts-ignore
+            session.user.id = token.id;
+
+            return session;
+        },
+
+        async signIn({profile, user, email, account, credentials}){
+            if(!process.env.EMAIL_DOMAIN){
+                throw "EMAIL_DOMAIN ENVIRONMENT VARIABLE MISSING"
+                return false;
+            }
+            return Boolean(profile?.email?.endsWith(process.env.EMAIL_DOMAIN));
+        }
+    },
     providers: [
         // @ts-expect-error You need to use .default here for it to work during SSR. May be fixed via Vite at some point
         GoogleProvider.default({
             clientId: process.env.GOOGLE_CLIENT_ID,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+            authorization: "https://accounts.google.com/o/oauth2/auth?response_type=code&hd=" + process.env.EMAIL_DOMAIN
         })
     ]
 })
