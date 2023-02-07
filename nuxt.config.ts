d982010f45d71f7bf126b6e5e126780b198cd8f6
DreamingCodes <dev@dreaming.codes>
2023-02-07 01:45:49 +0100

    Updated configs to match actual school timetable

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -20,8 +20,8 @@ export default defineNuxtConfig({
         "TITLE": "FORUM MEUCCI",
         "LANG": "it",
         "DESCRIPTION": "Pagina di registrazione per il forum dell'ITIS Meucci 2023",
-        "DAYS": ["Lunedì", "Martedì", "Mercoledì"],
-        "HOURS": ["8:00", "9:00"],
+        "DAYS": ["Martedì", "Mercoledì", "Giovedì"],
+        "HOURS": ["8:30", "11:00"],
     },
     nitro: {
       compressPublicAssets: true,
