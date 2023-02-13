ff7ff7eecbb7ccde8ce313004a32113b33932fe7
DreamingCodes <dev@dreaming.codes>
2023-02-14 00:37:44 +0100

    Vai instead of okay

--- pages/index.vue
+++ pages/index.vue
@@ -78,7 +78,7 @@ function openModal() {
               type="button"
               @click="router.push('/interactive')"
           >
-            OKAY
+            VAI
           </button>
         </div>
       </Dialog>
