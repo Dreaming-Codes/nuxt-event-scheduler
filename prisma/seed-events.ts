cf455327f0954169bc0a769af72c290a8a803719
DreamingCodes <dev@dreaming.codes>
2023-02-14 20:56:55 +0100

    Fixed top level await

--- prisma/seed-events.ts
+++ prisma/seed-events.ts
@@ -5,36 +5,32 @@ const prisma = new PrismaClient();
 
 const args = process.argv.slice(2);
 
-async function main(){
-    console.log("Import started")
-    console.log(args)
-    const file = await fs.readFile(args[0], 'utf8');
+console.log("Import started")
+console.log(args)
+const file = await fs.readFile(args[0], 'utf8');
 
-    const promises = [];
+const promises = [];
 
-    let parsed = 0;
-    let done = 0;
+let parsed = 0;
+let done = 0;
 
-    for (const line of file.split("\n")) {
-        const values = line.split("\t").map((value) => value.trim());
-        parsed++;
-        promises.push(new Promise(async ()=>{
-            await prisma.event.create({
-                data: {
-                    name: `[${values[0]}] ${values[1]}`,
-                    description: values[2],
-                    maxUsers: parseInt(values[3]) || 0,
-                }
-            });
-            done++;
-            const percentage = Math.round((done / parsed) * 100);
-            console.log(`${done}/${parsed}, ${percentage}%`)
-        }));
-    }
-
-    await Promise.all(promises);
-
-    console.log("Import completed")
+for (const line of file.split("\n")) {
+    const values = line.split("\t").map((value) => value.trim());
+    parsed++;
+    promises.push(new Promise(async () => {
+        await prisma.event.create({
+            data: {
+                name: `[${values[0]}] ${values[1]}`,
+                description: values[2],
+                maxUsers: parseInt(values[3]) || 0,
+            }
+        });
+        done++;
+        const percentage = Math.round((done / parsed) * 100);
+        console.log(`${done}/${parsed}, ${percentage}%`)
+    }));
 }
 
-main();
+await Promise.all(promises);
+
+console.log("Import completed")
--- prisma/seed-users.ts
+++ prisma/seed-users.ts
@@ -5,45 +5,41 @@ const prisma = new PrismaClient();
 
 const args = process.argv.slice(2);
 
-async function main(){
-    console.log("Import started")
-    console.log(args)
-    const file = await fs.readFile(args[0], 'utf8');
+console.log("Import started")
+console.log(args)
+const file = await fs.readFile(args[0], 'utf8');
 
-    const promises = [];
+const promises = [];
 
-    let parsed = 0;
-    let done = 0;
+let parsed = 0;
+let done = 0;
 
-    for (const line of file.split("\n")) {
-        const values = line.split(",").map((value) => value.trim());
-        parsed++;
-        const className = values[1].substring(1, values[1].length).replaceAll('-','');
-        const section = parseInt(values[1][0]);
-        promises.push(new Promise(async ()=>{
-            await prisma.user.upsert({
-                where: {
-                    email: values[0],
-                },
-                update: {
-                    class: className,
-                    section,
-                },
-                create: {
-                    email: values[0],
-                    section,
-                    class: className,
-                }
-            });
-            done++;
-            const percentage = Math.round((done / parsed) * 100);
-            console.log(`${done}/${parsed}, ${percentage}%`)
-        }));
-    }
-
-    await Promise.all(promises);
-
-    console.log("Import completed")
+for (const line of file.split("\n")) {
+    const values = line.split(",").map((value) => value.trim());
+    parsed++;
+    const className = values[1].substring(1, values[1].length).replaceAll('-', '');
+    const section = parseInt(values[1][0]);
+    promises.push(new Promise(async () => {
+        await prisma.user.upsert({
+            where: {
+                email: values[0],
+            },
+            update: {
+                class: className,
+                section,
+            },
+            create: {
+                email: values[0],
+                section,
+                class: className,
+            }
+        });
+        done++;
+        const percentage = Math.round((done / parsed) * 100);
+        console.log(`${done}/${parsed}, ${percentage}%`)
+    }));
 }
 
-main();
+await Promise.all(promises);
+
+console.log("Import completed")
