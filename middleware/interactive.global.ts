3e2dd7401c5c6d6cb657d4e8b2accbee23d1d356
DreamingCodes <dev@dreaming.codes>
2023-02-16 03:41:16 +0100

    Fixed another signIn error

--- middleware/interactive.global.ts
+++ middleware/interactive.global.ts
@@ -4,12 +4,20 @@ export default defineNuxtRouteMiddleware((to) => {
     return;
   }
 
-  const { status, data } = useSession();
+  const { status, signIn, data } = useSession();
 
   if (status.value === 'authenticated') {
     // @ts-ignore
     if (!data?.value?.user.interactiveDone && to.name !== 'interactive') {
       return navigateTo('/interactive');
     }
+    return;
   }
+
+  /**
+     * We cannot directly call and/or return `signIn` here as `signIn` uses async composables under the hood, leading to "nuxt instance undefined errors", see https://github.com/nuxt/framework/issues/5740#issuecomment-1229197529
+     *
+     * So to avoid calling it, we return it immeadiatly.
+     */
+  return signIn(undefined, { callbackUrl: to.path }) as ReturnType<typeof navigateTo>
 });
