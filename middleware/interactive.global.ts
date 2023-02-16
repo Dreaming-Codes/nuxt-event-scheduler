f90bbcdbdd475f2d4043d1ef381ee478ed1471e2
DreamingCodes <dev@dreaming.codes>
2023-02-16 03:25:16 +0100

    Added fix for countdown page

--- middleware/interactive.global.ts
+++ middleware/interactive.global.ts
@@ -1,18 +1,15 @@
 export default defineNuxtRouteMiddleware((to) => {
-  const { status, signIn, data } = useSession();
+  // Ignoring the countdown page
+  if (to.name === 'countdown') {
+    return;
+  }
+
+  const { status, data } = useSession();
 
   if (status.value === 'authenticated') {
     // @ts-ignore
     if (!data?.value?.user.interactiveDone && to.name !== 'interactive') {
       return navigateTo('/interactive');
     }
-    return;
   }
-
-  /**
-     * We cannot directly call and/or return `signIn` here as `signIn` uses async composables under the hood, leading to "nuxt instance undefined errors", see https://github.com/nuxt/framework/issues/5740#issuecomment-1229197529
-     *
-     * So to avoid calling it, we return it immeadiatly.
-     */
-  return signIn(undefined, { callbackUrl: to.path }) as ReturnType<typeof navigateTo>;
 });
