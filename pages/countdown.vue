ff5f11795e61360b0ff4f47a232bde1645af9051
DreamingCodes <dev@dreaming.codes>
2023-02-19 22:28:17 +0100

    Removed eslint

--- pages/countdown.vue
+++ pages/countdown.vue
@@ -1,4 +1,4 @@
-<script setup lang="ts">
+<script lang="ts" setup>
 const config = useAppConfig();
 
 const dateToCountDown = new Date(config.COUNTDOWN_DAY);
@@ -34,25 +34,25 @@ const seconds = computed(() => {
     <div class="grid grid-flow-col gap-5 text-center auto-cols-max">
       <div class="flex flex-col">
         <span class="countdown font-mono text-[22vmin]">
-          <span :style="`--value:${days}`" />
+          <span :style="`--value:${days}`"/>
         </span>
         Giorni
       </div>
       <div class="flex flex-col">
         <span class="countdown font-mono text-[18vmin]">
-          <span :style="`--value:${hours}`" />
+          <span :style="`--value:${hours}`"/>
         </span>
         Ore
       </div>
       <div class="flex flex-col">
         <span class="countdown font-mono text-[14vmin]">
-          <span :style="`--value:${minutes}`" />
+          <span :style="`--value:${minutes}`"/>
         </span>
         Minuti
       </div>
       <div class="flex flex-col">
         <span class="countdown font-mono text-[10vmin]">
-          <span :style="`--value:${seconds}`" />
+          <span :style="`--value:${seconds}`"/>
         </span>
         Secondi
       </div>
