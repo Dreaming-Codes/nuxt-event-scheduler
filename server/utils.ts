4a78da78bd0b78760405a83a9d7cb0deb9ab1efc
PolpOnline <git@t0mmy.anonaddy.com>
2023-02-20 01:09:41 +0100

    Utils.ts may be fixed now

--- server/utils.ts
+++ server/utils.ts
@@ -1,83 +1,76 @@
-import { H3Event } from 'h3';
-import { PrismaClient } from '@prisma/client';
-import { getServerSession } from '#auth';
+import {H3Event} from 'h3';
+import {PrismaClient} from '@prisma/client';
+import {getServerSession} from '#auth';
 
 export async function getSession(event: H3Event): Promise<Awaited<ReturnType<typeof import('#auth').getServerSession>>> {
-  const session = await getServerSession(event);
-  if (!session) {
-    throw createError({ statusMessage: 'Unauthenticated', statusCode: 403 });
-  }
-  return session;
+    const session = await getServerSession(event);
+    if (!session) {
+        throw createError({statusMessage: 'Unauthenticated', statusCode: 403});
+    }
+    return session;
 }
 
 export function checkParams(params: Record<string, string>, expectedParams: string[]) {
-  let missingParams = [];
-  if (!params) {
-    missingParams = expectedParams;
-  } else {
-    for (const param of expectedParams) {
-      if (params[param] == null) {
-        missingParams.push(param);
-      }
+    let missingParams = [];
+    if (!params) {
+        missingParams = expectedParams;
+    } else {
+        for (const param of expectedParams) {
+            if (params[param] == null) {
+                missingParams.push(param);
+            }
+        }
     }
-  }
-  if (missingParams.length > 0) {
-    throw createError({ statusMessage: `Missing parameters: ${missingParams.join(', ')}`, statusCode: 400 });
-  }
+    if (missingParams.length > 0) {
+        throw createError({statusMessage: `Missing parameters: ${missingParams.join(', ')}`, statusCode: 400});
+    }
 }
 
 export function getSlots(prisma: PrismaClient, round = NaN, eventId = NaN, ignoreUser = NaN) {
-  return prisma.eventUser.groupBy({
-    by: ['round', 'eventId'],
-    where: {
-      round: isNaN(round) ? undefined : round,
-      eventId: isNaN(eventId) ? undefined : eventId,
-      userId: isNaN(ignoreUser) ? undefined : { not: ignoreUser }
-    },
-    _count: true
-  });
+    return prisma.eventUser.groupBy({
+        by: ['round', 'eventId'],
+        where: {
+            round: isNaN(round) ? undefined : round,
+            eventId: isNaN(eventId) ? undefined : eventId,
+            userId: isNaN(ignoreUser) ? undefined : {not: ignoreUser}
+        },
+        _count: true
+    });
 }
 
 export function getCurrentRound(eventDay: string, hours: string[], hourLength: number) {
-  const startDate = new Date(eventDay);
-  let startDay = startDate.getDay() - 1;
-  if (startDay === -1) {
-    startDay = 6;
-  } // Sunday is 6, not 0
+    const startDate = new Date(eventDay);
+    let startDay = startDate.getDay() - 1;
+    if (startDay === -1) {
+        startDay = 6;
+    } // Sunday is 6, not 0
 
-  const now = new Date();
-  const currentHour = now.getHours() + now.getMinutes() / 60 + now.getSeconds() / 3600;
-  let currentDay = now.getDay() - 1;
-  if (currentDay === -1) {
-    currentDay = 6;
-  } // Again, sunday is 6, not 0
+    const now = new Date();
+    const currentHour = now.getHours() + now.getMinutes() / 60 + now.getSeconds() / 3600;
+    let currentDay = now.getDay() - 1;
+    if (currentDay === -1) {
+        currentDay = 6;
+    } // Again, sunday is 6, not 0
 
-  // Parse hours in config
-  const parsedHours = [];
-  for (const i in hours) {
-    const CompleteHour = hours[i].split(':');
-    parsedHours.push(Number(CompleteHour[0]) + Number(CompleteHour[1]) / 60);
-  }
+    // Parse hours in config
+    const parsedHours = [];
+    for (const i in hours) {
+        const CompleteHour = hours[i].split(':');
+        parsedHours.push(Number(CompleteHour[0]) + Number(CompleteHour[1]) / 60);
+    }
 
-  if (currentHour - parsedHours[0] < 0) {
-    throw createError({ statusMessage: 'The user is too early', statusCode: 403 });
-  }
-  if (currentHour - parsedHours[parsedHours.length - 1] > hourLength) {
-    throw createError({ statusMessage: 'The user is too late', statusCode: 403 });
-  }
+    if (currentHour - parsedHours[0] < 0) {
+        throw createError({statusMessage: 'The user is too early', statusCode: 403});
+    }
+    if (currentHour - parsedHours[parsedHours.length - 1] > hourLength) {
+        throw createError({statusMessage: 'The user is too late', statusCode: 403});
+    }
 
-  const roundHour = parsedHours.findIndex((hour) => {
-    return currentHour > hour && currentHour < hour + hourLength;
-  });
+    const roundHour = parsedHours.findIndex((hour) => {
+        return currentHour > hour && currentHour < hour + hourLength;
+    });
 
-  const roundDay = currentDay - startDay;
+    const roundDay = currentDay - startDay;
 
-  function roundDayAndHourToRoundNumber(day: number, hour: number) {
-    const roundsInADay = hours.length;
-    const cycleStart = day * roundsInADay;
-    const cyclePosition = hour + 1;
-    return cycleStart + cyclePosition;
-  }
-
-  return roundDayAndHourToRoundNumber(roundDay, roundHour);
+    return roundDay * hours.length + roundHour + 1;
 }
