cfa5ae3cad2c96e293debe512302c5856f342299
PolpOnline <git@t0mmy.anonaddy.com>
2023-02-20 16:06:07 +0100

    Fix some appearence issues on admin page

--- pages/admin/[id].vue
+++ pages/admin/[id].vue
@@ -19,8 +19,8 @@ const event = computed(() => globalStore.events.find(
     event => event.id === eventId.value
 ));
 
-if(!event.value) {
-  throw new Error("Event not found");
+if (!event.value) {
+  throw new Error("No event was found");
 }
 
 function sendPresence(e: Event, user: ArrElement<NonNullable<typeof users.value>["users"]>) {
@@ -33,17 +33,25 @@ function sendPresence(e: Event, user: ArrElement<NonNullable<typeof users.value>
   });
 }
 
+function capitalizeEachWord(str: string) {
+  return str.toLowerCase().split(" ").map(word => word[0].toUpperCase() + word.slice(1)).join(" ");
+}
+
 // checkPresence() {}
 </script>
 
 <template>
-  <div
-      class="white-transparent-component absolute translate-x-1/2 w-[90%] right-1/2"
-  >
+  <div class="white-transparent-component absolute translate-x-1/2 w-[90%] right-1/2">
     <!-- TODO: Here we need to show the round day and hour -->
     Sei sulla pagina del corso {{ event.name }}.
-    <div v-for="user in users?.users">
-      {{ user.user.name }} <input type="checkbox" class="toggle" :checked="user.user.checked" @change="e => sendPresence(e, user)"/>
+    <div v-for="(user, index) in users?.users" :key="index"
+         class="flex items-center p-2 mb-4">
+      <!-- for some reason, capitalize property does not work -->
+      <div class="w-full capitalize">{{ capitalizeEachWord(user.user.name) || user.user.email }}</div>
+      <div class="flex items-center ml-auto">
+        <input type="checkbox" class="toggle" :checked="user.user.checked"
+               @change="e => sendPresence(e, user)"/>
+      </div>
     </div>
   </div>
 </template>
