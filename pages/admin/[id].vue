5a5d824c74827ff5f431f95003be30a38f133a8f
PolpOnline <git@t0mmy.anonaddy.com>
2023-02-21 01:07:55 +0100

    Little adjustments to admin page

--- pages/admin/[id].vue
+++ pages/admin/[id].vue
@@ -36,18 +36,30 @@ function sendPresence(e: Event, user: ArrElement<NonNullable<typeof users.value>
 function capitalizeEachWord(str: string) {
   return str.toLowerCase().split(" ").map(word => word[0].toUpperCase() + word.slice(1)).join(" ");
 }
+
+/**
+ * Removes the leading [room] and the trailing dot from the event name
+ * @param str event name
+ */
+function sanitizeEventName(str: string) {
+  return str.replace(/^\[.*?]\s*/, '').replace(/\.$/, '')
+}
 </script>
 
 <template>
   <div class="white-transparent-component absolute translate-x-1/2 w-[90%] right-1/2">
-    <!-- TODO: Here we need to show the round day and hour -->
-    Sei sulla pagina del corso {{ event.name }}.
-    <div v-for="(user, index) in users?.users" :key="index"
-         class="flex items-center p-2 mb-4">
-      <!-- for some reason, capitalize property does not work -->
-      <div class="w-full">{{ capitalizeEachWord(user.user.name) || user.user.email }}</div>
+    Sei sulla pagina di appello del corso "{{ sanitizeEventName(event.name) }}".
+    <div class="flex items-start p-2 mb-1 font-black pb-0 text-xl">
+      <div class="w-full text-left">Nome</div>
       <div class="flex items-center ml-auto">
-        <input type="checkbox" class="toggle toggle-success color-red" :checked="user.user.checked"
+        Presente?
+      </div>
+    </div>
+    <div v-for="(user, index) in users?.users" :key="index"
+         class="flex items-center p-2 mb-1">
+      <div class="w-full text-left">{{ capitalizeEachWord(user.user.name) || user.user.email }}</div>
+      <div class="flex items-center ml-auto">
+        <input :checked="user.user.checked" class="toggle toggle-success color-red" type="checkbox"
                @change="e => sendPresence(e, user)"/>
       </div>
     </div>
