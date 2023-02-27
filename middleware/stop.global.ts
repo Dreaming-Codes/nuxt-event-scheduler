afe65437eb79264f411b45788798bfaa9c1f41d6
DreamingCodes <dev@dreaming.codes>
2023-02-28 00:10:58 +0100

    Fixed global stop

--- middleware/stop.global.ts
+++ middleware/stop.global.ts
@@ -3,7 +3,7 @@ export default defineNuxtRouteMiddleware((to) => {
 
     if (status.value === "authenticated") {
         // @ts-ignore
-        if (!data?.value?.user.interactiveDone && to.name !== "") {
+        if (to.name !== "") {
             return navigateTo("/");
         }
     }
