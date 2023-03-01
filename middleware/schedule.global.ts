5f00f1650407c8abcdb05686e7ec43168ca02316
PolpOnline <git@t0mmy.anonaddy.com>
2023-03-01 08:19:00 +0100

    Fix unprotected pages under schedule/*

--- middleware/schedule.global.ts
+++ middleware/schedule.global.ts
@@ -4,8 +4,8 @@ export default defineNuxtRouteMiddleware((to) => {
     // @ts-ignore
     if (data?.value?.user.admin) return;
     if (
-        status.value === "authenticated" &&
-        !to.fullPath.startsWith("/schedule")
+        (status.value === "authenticated" && to.path !== "/schedule") ||
+        (to.path !== "/schedule" && to.fullPath.startsWith("/schedule"))
     ) {
         return navigateTo("/schedule");
     }
