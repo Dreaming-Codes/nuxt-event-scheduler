ddf25c215f099129149fb99f934983114fbfa64a
Tommaso Melacarne <git@t0mmy.anonaddy.com>
2024-02-15 12:34:20 +0100

    Update seed-users.ts

--- prisma/seed-users.ts
+++ prisma/seed-users.ts
@@ -9,41 +9,45 @@ console.log("Import started");
 console.log(args);
 const file = await fs.readFile(args[0], "utf8");
 
-const promises = [];
+// const promises = [];
 
-let parsed = 0;
+let total = file.split("\n").length;
 let done = 0;
 
 for (const line of file.split("\n")) {
     const values = line.split(",").map((value) => value.trim());
-    parsed++;
-    const className = values[1]
-        .substring(1, values[1].length)
-        .replaceAll("-", "");
-    const section = parseInt(values[1][0]);
-    promises.push(
-        new Promise(async () => {
+    const name = values[2];
+    const email = values[3];
+    const section = parseInt(values[4]);
+    const className = values[5];
+
+    console.log(`Importing ${name} (${email}), section ${section}, class ${className}`);
+
+    // promises.push(
+    //     new Promise(async () => {
             await prisma.user.upsert({
                 where: {
-                    email: values[0]
+                    email
                 },
                 update: {
+                    name,
                     class: className,
                     section
                 },
                 create: {
-                    email: values[0],
+                    email,
+                    name,
                     section,
                     class: className
                 }
             });
             done++;
-            const percentage = Math.round((done / parsed) * 100);
-            console.log(`${done}/${parsed}, ${percentage}%`);
-        })
-    );
+            const percentage = Math.round((done / total) * 100);
+            console.log(`${done}/${total}, ${percentage}%`);
+    //     })
+    // );
 }
 
-await Promise.all(promises);
+// await Promise.all(promises);
 
 console.log("Import completed");
